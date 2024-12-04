51dac1475822e168f9c750cc8ce566dabf0871c8
Paax <Zacharybanks2000@gmail.com>
2024-12-04 05:31:22 -0500

    your commit message

--- main.tf
+++ main.tf
@@ -111,3 +111,22 @@ resource "docker_network" "lingo_network" {
   name = "lingo_network"
   driver = "bridge"
 }
+
+# Nginx
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  name  = "lingo-nginx"
+  image = docker_image.nginx.image_id
+  
+  ports {
+    internal = 80
+    external = 80
+  }
+  
+  networks_advanced {
+    name = docker_network.lingo_network.name
+  }
+}
